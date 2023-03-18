acc40c5ae133fcfc63e9cac3f8e5fb9647eadd5d
zonrajput <128221996+zonrajput@users.noreply.github.com>
2023-03-18 03:16:22 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Channel-Mdisk-Converter.git /Channel-Mdisk-Converter
+  git clone https://github.com/zonrajput/zone-mdisk
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /Channel-Mdisk-Converter
+  git clone $SOURCE_CODE /zone-mdisk
 fi
-cd /Channel-Mdisk-Converter
+cd /zone-mdisk
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
